f1c078d5f27b5d048aaba930718e88c28ad54ae5
Ethel-A <etheladongo98@gmail.com>
2018-11-14 14:03:11 +0000

    Extra work on sklearn logistic regression model

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,22 +1,54 @@
-#Using the sciKit learn naive Bayes library.
+from sklearn.feature_extraction.text import CountVectorizer;
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import sys
+import warnings
 
-'''Importing Libraries and Loading Datasets'''
-from sklearn import datasets; #imports datasets that will be used to test the classifier
-from sklearn import metrics; #imports the evaluation metrics that will be used to compare the classifiers
-from sklearn.naive_bayes import GaussianNB; #imports that Gaussian Naive Bayes Classifier - which is a naive bayes classifier that works on the assumption that the distribution is Gaussian (normal)
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+    
+'''Function to train unormalized data'''
+def trainer():
+  with open('bigTrain.txt') as train_data:
+    train_list = train_data.readlines()
+    data = []
+    labels = []
+  for line in train_list:
+    data.append(line[:-3])
+    labels.append(line[-2])
 
-dataset = datasets.load_iris(); # loads the iris dataset - which is a class sytem for iris plants
+  vectorizer = CountVectorizer(lowercase = False)
+  
+  normFeat = vectorizer.fit_transform(data)
+  normFeat = normFeat.toarray() # for easy usage
+  X_train, X_test, y_train, y_test  = train_test_split(
+          normFeat, 
+          labels,
+          train_size=0.80, 
+          random_state=1234)
+  return X_train, X_test, y_train, y_test, vectorizer
 
-'''Creating Naive-Bayes Classifier'''
-model = GaussianNB();#creates a model that works on the assumption that the distribution is gaussian
-model.fit(dataset.data, dataset.target); #fits the model to the dataset
+def modeler(X_train, y_train):
+  log_model = LogisticRegression()
+  log_model = log_model.fit(X_train, y_train)
+  return log_model
 
-'''Making Predictions'''
-expected = dataset.target;
-predicted = model.predict(dataset.data);#returns the predicted values of the iris dataset
+def tester(X_test, model):
+  y_pred = model.predict(X_test)
+  return y_pred
 
-'''Testing Accuracy and statisitics on dataset'''
-print(metrics.classification_report(expected,predicted));
-print(metrics.confusion_matrix(expected,predicted));
-
-#PROGRAM WORKS - NEXT STEP: CHANGE DATASET FOR SENTIMENT CLASSIFICATION
+def main(testfile):
+  with open(testfile) as testdata:
+      test = testdata.readlines()
+  vals = trainer()
+  X_train = vals[0]
+  X_test = vals[1]
+  y_train = vals[2]
+  y_test = vals[3]
+  vectorizer = vals[4]
+  test = vectorizer.fit_transform(test)
+  test = test.toarray()
+  model = modeler(X_train, y_train)
+  predictions = tester(test, model)
+  print(predictions)
+  
